0ef7aeb4813736494253a0c30c4973c191cc2f0f
Saman Ashoori <saman@samanashoori.com>
2025-11-29 23:59:29 +0000

    fix: correct parameter name in enqueue method and adjust initialization logic

--- Queue/circularqueue.py
+++ Queue/circularqueue.py
@@ -7,9 +7,14 @@ class Queue:
         #To simulate C++ memory management
         self.data = ['None'] * size
     
-    def enqueue(self,value:int) -> bool:
+    def enqueue(self,input:int) -> bool:
         #input a int but return a boolean value
         if(self.isEmpty == True):
-            self.front = 0
-            self.tail = 0
-            self.data
+            self.front = self.front + 1
+            self.tail = self.tail + 1
+            self.data[self.tail] = input
+            self.isEmpty = False
+            return True
+
+q1 = Queue(4)
+q1.enqueue(4)
